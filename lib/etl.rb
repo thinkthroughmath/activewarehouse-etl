54a7866fbeb424c5f0552f5cba9f7f80aceb25c6
Sandeep Arey <sandeep.arey@imaginelearning.com>
2026-06-08 11:09:45 +0530

    Use plain require 'rexml' (works on REXML 3.x, Ruby 3+)

--- lib/etl.rb
+++ lib/etl.rb
@@ -31,16 +31,9 @@ require 'erb'
 
 require 'rubygems'
 
+require 'rexml'
 unless defined?(REXML::VERSION)
-  # REXML 3.x (Ruby 3+) exposes the version via rexml/version. Earlier
-  # releases shipped rexml/rexml; fall back to the shim used by older code
-  # that still references REXML::Version.
-  begin
-    require 'rexml/version'
-  rescue LoadError
-    require 'rexml/rexml'
-  end
-  REXML::VERSION = REXML::Version unless defined?(REXML::VERSION)
+  REXML::VERSION = REXML::Version
 end
 
 require 'active_support'
